100de66e171bb09eed033926aeaf42cbe6774cca
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2015-10-27 10:56:09 +0100

    Definition of constants
    Defined the constants used in the test cases to test the system components.

---
+++ core/src/test/scala/org/citysimulator/core/constants/test/TestConstants.scala
@@ -0,0 +1,17 @@
+package org.citysimulator.core.constants.test
+
+/**
+ * Enumerates all the constants used in the test cases to test the system components
+ */
+object TestConstants {
+
+  /**
+   * Boolean constant false
+   */
+  val FALSE: Boolean = false
+
+  /**
+   * Boolean constant true
+   */
+  val TRUE: Boolean = true
+}
---
+++ core/src/test/scala/org/citysimulator/core/constants/test/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The package contains the constants used in the test case to test the system components
+ *
+ * @author andrea
+ * @version 1.0
+ */
+package org.citysimulator.core.constants.test;
