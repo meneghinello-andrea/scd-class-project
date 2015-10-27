755f888f7a6fd66e188df3a69785be00b905f2a3
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2015-10-27 14:55:05 +0100

    Documentation
    Provided the missing documentation on the class parameters.

--- core/src/main/scala/org/citysimulator/core/business/citizen/Citizen.scala
+++ core/src/main/scala/org/citysimulator/core/business/citizen/Citizen.scala
@@ -9,6 +9,7 @@ import org.citysimulator.core.business.vehicle.Vehicles.Vehicles
  * @param id [[String]] identifier of the [[Citizen]]
  * @param name [[String]] name of the [[Citizen]]
  * @param vehicle Vehicle used by the citizen when it travels in the city
+ * @param addressBook [[AddressBook]] where the [[Citizen]] save important address
  * @param status Information on what about the citizen is performing
  */
 case class Citizen(id: String,
