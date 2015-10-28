2b16328295fa13943e337104fa386324602fee7e
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2015-10-28 16:27:08 +0100

    Package definition
    Definition of the package that will contain the messages that the actors in the system exchange during system lifetime.

---
+++ core/src/main/scala/org/citysimulator/core/message/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The package contains the messages that the actors that participate in the system must manage in order to
+ * advance correctly the simulation
+ *
+ * @author andrea
+ * @version 1.0
+ */
+package org.citysimulator.core.message;
