7d3f0aac131371844b572859954f57305808bcd1
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2015-10-31 18:29:41 +0100

    Package constants
    Defined the package that will contain the constant values used by the system component.

---
+++ core/src/main/scala/org/citysimulator/core/constants/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The package contains the object that contains the constant values used by the core components
+ *
+ * @author andrea
+ * @version 1.0
+ */
+package org.citysimulator.core.constants;
