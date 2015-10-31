6ceaa264c9a83fbf2cdc3747330ed95b60b39dbb
Andrea Meneghinello <meneghinello.andrea@gmail.com>
2015-10-31 18:28:59 +0100

    Package actor
    Defined the package that will contains all the actor that compose the system.

---
+++ core/src/main/scala/org/citysimulator/core/actor/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The package contains the actor that the system uses to build and run the simulation autonomously
+ *
+ * @author andrea
+ * @version 1.0
+ */
+package org.citysimulator.core.actor;
